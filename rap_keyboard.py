e93cfa75e126d5b75743a312da3a239d7f43ac36
mars <nicolas2mars@gmail.com>
2019-04-24 16:00:23 +0200

    second commit

--- rap_keyboard.py
+++ rap_keyboard.py
@@ -4,7 +4,8 @@ import requests
 import sys
 import webbrowser
 import time
-import keyboard
+from pynput import keyboard
+#youtube dl
 
 def website(url):
 	result = requests.get(url)
@@ -31,32 +32,32 @@ def main():
 if __name__ == "__main__" :
 	main()
 
-
 entreeSon= input("Entrer un url d'une prod youtube : ")
 webbrowser.open(entreeSon)
-
-
 engine = pyttsx3.init()
-
-
 with open('/home/mars/Bureau/tweetstrump/texte.txt') as f:
         lines = f.read().splitlines()
-        time.sleep(25)
+        #time.sleep(25)
 
-voices = engine.getProperty('voices')       #getting details of current voice
-engine.setProperty('voice', 'french')
-#print(voices)
-#for voice in voices:
-#    print("Voice")
-rate = engine.getProperty('rate')
-engine.setProperty('rate', rate+30)
+def set_engine():
+	voices = engine.getProperty('voices')       #getting details of current voice
+	engine.setProperty('voice', 'french')
+	rate = engine.getProperty('rate')
+	engine.setProperty('rate', rate+30)
+	volume = engine.getProperty('volume')
+	engine.setProperty('volume', volume-0.75)
 
-volume = engine.getProperty('volume')
-engine.setProperty('volume', volume-0.75)
-"""
-engine.say(lines)
-engine.runAndWait()
-"""
-if keyboard.is_pressed('q'):
-    engine.say(lines)
-    engine.runAndWait()
+def get_key_name(key):
+    if isinstance(key, keyboard.KeyCode):
+        return key.char
+    else:
+        return str(key)
+
+def on_press(key):
+    key_name = get_key_name(key)
+    if key_name == "Key.space":
+        engine.say(lines)
+        engine.runAndWait()
+
+with keyboard.Listener(on_press = on_press) as listener:
+    listener.join()
